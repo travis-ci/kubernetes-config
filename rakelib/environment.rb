dbd7d354f0ef5a975eeeb95ac21532780254e9f2
Matt Moriarity <matt@mattmoriarity.com>
2018-11-06 12:44:35 -0600

    Add module system and make worker and JB modules

--- rakelib/environment.rb
+++ rakelib/environment.rb
@@ -1,4 +1,6 @@
 require 'rake/clean'
+require 'erb'
+require 'ostruct'
 require 'yaml'
 
 TOP = `git rev-parse --show-toplevel`.chomp
@@ -8,20 +10,38 @@ CONFIG = YAML.safe_load(File.read('env.yml'))
 ENV_NAME = File.basename(Dir.pwd)
 
 desc "Apply changes to Kubernetes cluster"
-task apply: :secrets do
-  sh "kubectl apply --context=#{ENV_NAME} -f secrets/"
-  sh "kubectl apply --context=#{ENV_NAME} -f templates/"
+task apply: :plan do
+  sh "kubectl apply --context=#{ENV_NAME} -f build/secrets/"
+  sh "kubectl apply --context=#{ENV_NAME} -R -f build/modules/"
+  sh "kubectl apply --context=#{ENV_NAME} -f build/templates/"
 end
 
-CLEAN << 'secrets'
+desc "Create files for resources to be applied"
+task plan: :secrets
+
+directory 'build'
+CLEAN << 'build'
+
+directory 'build/templates'
+
+local_templates = FileList['templates/*.yaml']
+local_templates.each do |template|
+  dest = "build/#{template}"
+  file dest => ['build/templates', template] do
+    cp template, dest
+  end
+
+  task plan: dest
+end
+
+directory 'build/secrets'
 
 def secret_filename(secret)
-  "secrets/#{secret['name']}.yaml"
+  "build/secrets/#{secret['name']}.yaml"
 end
 
 CONFIG.fetch('secrets', []).each do |secret|
-  file secret_filename(secret) do
-    mkdir_p 'secrets'
+  file secret_filename(secret) => ['build/secrets'] do
     if secret['file']
       sh make_secret_file_command(secret)
     else
@@ -55,4 +75,29 @@ def make_secret_file_command(secret)
   end
   file = "#{root}/#{secret['file']}"
   "#{MAKE_SECRET} -n #{secret['name']} -k #{secret['key']} #{file} > #{secret_filename(secret)}"
+end
+
+CONFIG.fetch('modules', []).each do |name, mod|
+  build_dir = "build/modules/#{name}"
+  directory build_dir
+
+  path = "../modules/#{mod['module']}"
+
+  module_hash = {}
+  module_hash.merge!(YAML.safe_load(File.read("#{path}/defaults.yml")))
+  module_hash.merge!(mod)
+  module_hash['name'] = name
+
+  templates = FileList["#{path}/templates/*.erb"]
+  templates.each do |template|
+    dest = template.pathmap("#{build_dir}/%n")
+    file dest => [build_dir, template] do
+      result = ERB.new(File.read(template)).result_with_hash(module_hash)
+      File.write(dest, result)
+    end
+
+    task "module:#{name}" => dest
+  end
+
+  task plan: "module:#{name}"
 end
