5ffda79b3ac9fa3e23d069db58e008287d0fb93e
Matt Moriarity <matt@mattmoriarity.com>
2018-11-13 12:42:48 -0600

    Add production release for jupiter-brain-org
    This is largely to test that we can only pull in releases for the right
    environment. We do this by configuring Flux to only look for resources
    in that releases directory.

--- shared/install-flux.sh
+++ shared/install-flux.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+NAMESPACE=$(kubectl config current-context)
+
 helm repo add weaveworks https://weaveworks.github.io/flux
 helm upgrade flux weaveworks/flux \
   --set rbac.create=true \
   --set helmOperator.create=true \
   --set git.url=ssh://git@github.com/travis-ci/kubernetes-config.git \
   --set git.branch=mjm-flux \
+  --set "git.path=releases/$NAMESPACE" \
   --set git.chartsPath=charts \
   --set git.pollInterval=1m \
   --namespace flux
