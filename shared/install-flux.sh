56a2b10aa3545e1235739e8ee361d9e096c09226
Matt Moriarity <matt@mattmoriarity.com>
2018-11-13 12:25:39 -0600

    Add Flux release for jupiter-brain-com in staging

--- shared/install-flux.sh
+++ shared/install-flux.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 helm repo add weaveworks https://weaveworks.github.io/flux
-helm install --name flux \
+helm upgrade flux weaveworks/flux \
   --set rbac.create=true \
   --set helmOperator.create=true \
   --set git.url=ssh://git@github.com/travis-ci/kubernetes-config.git \
   --set git.branch=mjm-flux \
   --set git.chartsPath=charts \
-  --namespace flux \
-  weaveworks/flux
+  --set git.pollInterval=1m \
+  --namespace flux
