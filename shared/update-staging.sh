09efbce0fb4ce346794f6b4bdb3e802c8c2bc843
Matt Moriarity <matt@mattmoriarity.com>
2018-11-30 13:37:01 -0600

    Automatically update staging branch for PRs

---
+++ shared/update-staging.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+merge_to_staging() {
+  git checkout staging
+  git merge "$1"
+  git push origin staging
+  git checkout -
+}
+
+merge_master_to_staging() {
+  echo "Merging master branch to staging"
+  merge_to_staging "$TRAVIS_COMMIT"
+}
+
+merge_pr_to_staging() {
+  echo "Merging PR to staging"
+  merge_to_staging "$TRAVIS_PULL_REQUEST_SHA"
+}
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  if [[ "$TRAVIS_PULL_REQUEST_SLUG" == "travis-ci/kubernetes-config" ]]; then
+    merge_pr_to_staging
+  fi
+elif [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  merge_master_to_staging
+fi
