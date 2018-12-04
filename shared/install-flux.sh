bfe851ab8dbeb885cfa9faf93ec57c7c100065a3
Matt Moriarity <matt@mattmoriarity.com>
2018-12-04 09:14:50 -0600

    Go back to using SSH URLs for Git with Flux

--- shared/install-flux.sh
+++ shared/install-flux.sh
@@ -12,7 +12,7 @@ helm upgrade flux weaveworks/flux \
   --install \
   --set rbac.create=true \
   --set helmOperator.create=true \
-  --set git.url=https://github.com/travis-ci/kubernetes-config.git \
+  --set git.url=git@github.com:travis-ci/kubernetes-config.git \
   --set git.branch="$BRANCH" \
   --set "git.path=releases/$NAMESPACE" \
   --set git.pollInterval=1m \
