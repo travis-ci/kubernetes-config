283788918f7170d1201a33389f862a1eb4bf2b03
Matt Moriarity <matt@mattmoriarity.com>
2018-12-04 09:01:05 -0600

    Use staging branch for Flux in staging environments

--- shared/install-flux.sh
+++ shared/install-flux.sh
@@ -2,12 +2,18 @@
 
 NAMESPACE=$(kubectl config current-context)
 
+BRANCH="master"
+if [[ $NAMESPACE == *staging* ]]; then
+  BRANCH="staging"
+fi
+
 helm repo add weaveworks https://weaveworks.github.io/flux
 helm upgrade flux weaveworks/flux \
   --install \
   --set rbac.create=true \
   --set helmOperator.create=true \
   --set git.url=https://github.com/travis-ci/kubernetes-config.git \
+  --set git.branch="$BRANCH" \
   --set "git.path=releases/$NAMESPACE" \
   --set git.pollInterval=1m \
   --namespace flux
